283c7965dc2884fd5df108ddf7d843b4a57b77f6
oktamorph <oktamorph@gmail.com>
2022-04-05 15:05:11 +0200

    add the "SingletonPattern" project

---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,60 @@
+﻿namespace SingletonPattern
+{
+    public class Singleton
+    {
+        // Lazy instantiation of the Singleton Pattern.
+        private static Singleton _uniqueInstance;
+        private Singleton() { }
+        public static Singleton GetInstance()
+        {
+            lock (_uniqueInstance)
+            {
+                if (_uniqueInstance == null)
+                {
+                    _uniqueInstance = new Singleton();
+                }
+                return _uniqueInstance;
+            }
+        }
+
+        // Eagerly instantiation of the Singleton Pattern.
+        //private static Singleton _uniqueInstance = new Singleton();
+        //private Singleton() { }
+        //public static Singleton GetInstance()
+        //{
+        //    return _uniqueInstance;
+        //}
+
+        // Double-checked locking to reduce the use of synchronization.
+        //private volatile static Singleton _uniqueInstance;
+        //private Singleton() { }
+        //public static Singleton GetInstance()
+        //{
+        //    if (_uniqueInstance == null)
+        //    {
+        //        lock (_uniqueInstance)
+        //        {
+        //            if (_uniqueInstance == null)
+        //            {
+        //                _uniqueInstance = new Singleton();
+        //            }
+        //        }
+        //    }
+        //    return _uniqueInstance;
+        //}
+
+    }
+
+    // Implementation of the Singleton Pattern using enum.
+    //public enum Singletons
+    //{
+    //    UNIQUE_INSTANCE
+    //}
+    //public class SingletonClient
+    //{
+    //    public static void Main(string[] args)
+    //    {
+    //        Singletons singleton = Singletons.UNIQUE_INSTANCE;
+    //    }
+    //}
+}
