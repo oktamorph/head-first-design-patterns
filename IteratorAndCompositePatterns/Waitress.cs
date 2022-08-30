7de038e6723e7b34866ca94867f5801ca1bb0665
oktamorph <oktamorph@gmail.com>
2022-08-30 20:16:28 +0200

    update the 'Waitress' class

--- IteratorAndCompositePatterns/Waitress.cs
+++ IteratorAndCompositePatterns/Waitress.cs
@@ -2,30 +2,44 @@
 {
     public class Waitress
     {
-        private readonly IMenu _pancakeHouseMenu;
-        private readonly IMenu _dinerMenu;
-        private readonly IMenu _cafeMenu;
+        //private readonly IMenu _pancakeHouseMenu;
+        //private readonly IMenu _dinerMenu;
+        //private readonly IMenu _cafeMenu;
 
-        public Waitress(IMenu pancakeHouseMenu, IMenu dinerMenu, IMenu cafeMenu)
+        //public Waitress(IMenu pancakeHouseMenu, IMenu dinerMenu, IMenu cafeMenu)
+        //{
+        //    this._pancakeHouseMenu = pancakeHouseMenu;
+        //    this._dinerMenu = dinerMenu;
+        //    this._cafeMenu = cafeMenu;
+        //}
+        //public void PrintMenu()
+        //{
+        //    Iterator<MenuItem> pancakeIterator = _pancakeHouseMenu.CreateIterator();
+        //    Iterator<MenuItem> dinerIterator = _dinerMenu.CreateIterator();
+        //    Iterator<MenuItem> cafeIterator = _cafeMenu.CreateIterator();
+
+        //    Console.WriteLine("MENU\n----\nBREAKFAST");
+        //    PrintMenu(pancakeIterator);
+
+        //    Console.WriteLine("\nLUNCH");
+        //    PrintMenu(dinerIterator);
+
+        //    Console.WriteLine("\nDINNER");
+        //    PrintMenu(cafeIterator);
+        //}
+        private readonly List<IMenu> _menus;
+        public Waitress(List<IMenu> menus)
         {
-            this._pancakeHouseMenu = pancakeHouseMenu;
-            this._dinerMenu = dinerMenu;
-            this._cafeMenu = cafeMenu;
+            this._menus = menus;
         }
         public void PrintMenu()
         {
-            Iterator<MenuItem> pancakeIterator = _pancakeHouseMenu.CreateIterator();
-            Iterator<MenuItem> dinerIterator = _dinerMenu.CreateIterator();
-            Iterator<MenuItem> cafeIterator = _cafeMenu.CreateIterator();
-
-            Console.WriteLine("MENU\n----\nBREAKFAST");
-            PrintMenu(pancakeIterator);
-
-            Console.WriteLine("\nLUNCH");
-            PrintMenu(dinerIterator);
-
-            Console.WriteLine("\nDINNER");
-            PrintMenu(cafeIterator);
+            var menuIterator = _menus.GetEnumerator();
+            while (menuIterator.MoveNext())
+            {
+                IMenu menu = menuIterator.Current;
+                PrintMenu(menu.CreateIterator());
+            }
         }
         private void PrintMenu(Iterator<MenuItem> iterator)
         {
