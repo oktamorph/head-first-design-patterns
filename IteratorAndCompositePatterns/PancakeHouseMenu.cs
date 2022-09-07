a7830eb721c67d3b250f264760cf08bbff0e73eb
oktamorph <oktamorph@gmail.com>
2022-09-07 14:17:40 +0200

    refactor the 'PancakeHouseMenu' class

--- IteratorAndCompositePatterns/PancakeHouseMenu.cs
+++ IteratorAndCompositePatterns/PancakeHouseMenu.cs
@@ -2,10 +2,9 @@
 {
     public class PancakeHouseMenu : IMenu
     {
-        private readonly List<MenuItem> _menuItems;
+        private readonly Dictionary<string, MenuItem> _menuItems = new Dictionary<string, MenuItem>();
         public PancakeHouseMenu()
         {
-            _menuItems = new List<MenuItem>();
             AddItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs and toast", true, 2.99);
             AddItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
             AddItem("Blueberry Pancakes", "Pancakes made with fresh blueberries", true, 3.49);
@@ -14,18 +13,20 @@
         public void AddItem(string name, string description, bool vegetarian, double price)
         {
             var menuItem = new MenuItem(name, description, vegetarian, price);
-            _menuItems.Add(menuItem);
+            _menuItems.Add(name, menuItem);
         }
         public Iterator<MenuItem> CreateIterator()
         {
             var items = new MenuItem[_menuItems.Count];
-            
-            for(var i = 0; i < items.Length; i++)
+            var counter = 0;
+
+            foreach (var item in _menuItems)
             {
-                items[i] = _menuItems[i];
+                items[counter] = item.Value;
+                counter++;
             }
 
-            return new PancakeHouseMenuIterator(items);
+            return new CafeMenuIterator(items);
         }
     }
 }
