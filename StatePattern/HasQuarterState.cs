61338441bb7fa88f1fec79ef516ac0bc34ba363d
oktamorph <oktamorph@gmail.com>
2022-10-18 08:27:31 +0200

    implement the functionality for the 'HasQuarterState' class

--- StatePattern/HasQuarterState.cs
+++ StatePattern/HasQuarterState.cs
@@ -1,6 +1,29 @@
 ﻿namespace StatePattern
 {
-    internal class HasQuarterState
+    public class HasQuarterState : IState
     {
+        private readonly GumballMachine _gumballMachine;
+        public HasQuarterState(GumballMachine gumballMachine)
+        {
+            this._gumballMachine = gumballMachine;
+        }
+        public void InsertQuarter()
+        {
+            Console.WriteLine("You can't insert another quarter");
+        }
+        public void EjectQuarter()
+        {
+            Console.WriteLine("Quarter returned");
+            _gumballMachine.SetState(_gumballMachine.GetNoQuarterState());
+        }
+        public void TurnCrank()
+        {
+            Console.WriteLine("You turned...");
+            _gumballMachine.SetState(_gumballMachine.GetSoldState());
+        }
+        public void Dispense()
+        {
+            Console.WriteLine("No gumball dispensed");
+        }
     }
 }
